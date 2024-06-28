ad260a643f1f65720530ee984345887221d6dd8f
devGiyosiddin <devgiyosiddin@gmail.com>
2024-06-28 21:34:41 +0500

    debugging-1-done-1

---
+++ Public/js/script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('.header-input');
+const inputWrapper = document.querySelector('.input-wrapper');
+const searchIcon = document.querySelector('.search-icon');
+
+input.addEventListener('input', function() {
+    if (input.value.trim() !== '') {
+        inputWrapper.classList.add('has-text');
+    } else {
+        inputWrapper.classList.remove('has-text');
+    }
+});
+
+input.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        // Действие по нажатию клавиши Enter (например, отправка формы)
+        console.log(input.value);
+    }
+});
+
+searchIcon.addEventListener('click', function () {
+    input.value = '';
+    inputWrapper.classList.remove('has-text');
+});
