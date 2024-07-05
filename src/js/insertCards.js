31e9d0ad3fd62f9fd76d0e54370671395cf5707c
devGiyosiddin <devgiyosiddin@gmail.com>
2024-07-05 18:34:52 +0500

    debugging-2-done-1

---
+++ src/js/insertCards.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const weatherData = [
+        {
+            title: "Влажность",
+            icon: "humidity",
+            value: "75 %",
+            minParams: "0%",
+            maxParams: "100%",
+            range: true,
+            humidity: true,
+        },
+        {
+            title: "Давление",
+            icon: "barometr",
+            value: "761",
+            minParams: "0%",
+            maxParams: "1000%",
+            param: "Повышенное",
+            pressure: true,
+            range: true,
+        },
+        {
+            title: "Видимость",
+            icon: "visibility",
+            value: "28 км",
+            minParams: "0%",
+            maxParams: "100%",
+            param: "Нормальная",
+            range: true,
+        },
+        {
+            title: "Рассвет",
+            icon: "sunrise",
+            value: "8:42",
+            param: "Прошло: ",
+            time: "02:47",
+        },
+        {
+            title: "Закат",
+            icon: "sunset",
+            value: "16:37",
+            param: "Осталось: ",
+            time: "05:08",
+        },
+        {
+            title: "Сила ветра",
+            icon: "direction",
+            value: "2 м/с",
+            param: "Северо-западный",
+        },
+    ];
+
+    const cardsContainer = document.getElementById('cards');
+    const cardsHTML = weatherData.map((card) => {
+        return `<li class="card">
+            <div class="card-content">
+                <h3 class="card-title">${card.title}</h3>
+                <img src="img/cards-icon/${card.icon}.svg" alt="icon" class="card-icon" width="32" height="32">
+                <span class="card-value">${card.value}</span>
+            </div>
+            <div class="card-footer">
+                ${card.range ? `<input type="range" min="${parseInt(card.minParams)}" max="${parseInt(card.maxParams)}" value="${parseInt(card.value)}" class="slider-input ${card.pressure ? 'slider-input--pressure' : ''}">` : ''}
+                ${card.humidity
+                ? `<div class="card-footer--humidity">
+                        <span class="card-footer__from">${card.minParams}</span>
+                        <span class="card-footer__to">${card.maxParams}</span>
+                    </div>`
+                : `${card.param || ''} ${card.time || ''}`}
+                </div>
+        </li>`;
+    }).join("");
+
+    cardsContainer.innerHTML = cardsHTML;
+});
